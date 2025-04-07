460727321408ca4e50682ed48f6c6d03a1dea48e
ShahzadaFahimJan <shahzadafahimjan2001@gmail.com>
2025-04-07 16:53:22 +0500

    first commit

---
+++ first.py
@@ -0,0 +1,28 @@
+print("hello")
+
+"""
+
+
+import statistics
+data =[2,3,4,56,3,35,5,3]
+
+
+me= statistics.mean(data)
+std_dev = statistics.stdev(data)
+variancee__ = statistics.variance(data)
+Range = max(data) - min(data)
+print("the range of the data is ", Range)
+print(me,std_dev,variancee__)
+
+"""
+import statistics
+data = [2,4,5,6,7,8,2,4,6,7,77,77,88]
+
+
+mean_data = statistics.mean(data)
+std_dev = statistics.stdev(data)
+CV = (std_dev/mean_data) * 100
+print(
+" the coffiecient of vairation is ", CV
+)
+
