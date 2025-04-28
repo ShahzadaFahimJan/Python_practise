7200647604ab13d6cd3c8cef36c8a2033b8d746e
ShahzadaFahimJan <shahzadafahimjan2001@gmail.com>
2025-04-28 17:06:20 +0500

    message

--- first.py
+++ first.py
@@ -1,38 +1,13 @@
-print("hello")
+import nltk
+nltk.download('punkt')
+nltk.download('averaged_perceptron_tagger')
 
-"""
+from nltk.tokenize import word_tokenize
+from nltk import pos_tag
 
+text = "Fahim loves programming in Python."
+words = word_tokenize(text)
 
-import statistics
-data =[2,3,4,56,3,35,5,3]
-
-
-me= statistics.mean(data)
-std_dev = statistics.stdev(data)
-variancee__ = statistics.variance(data)
-Range = max(data) - min(data)
-print("the range of the data is ", Range)
-print(me,std_dev,variancee__)
-
-"""
-import statistics
-data = [2,4,5,6,7,8,2,4,6,7,77,77,88]
-
-
-mean_data = statistics.mean(data)
-std_dev = statistics.stdev(data)
-CV = (std_dev/mean_data) * 100
-print(
-" the coffiecient of vairation is ", CV
-)
-
-
-print("hello world : ")
-print("************")
-print("**********")
-print("*******")
-print("****")
-
-
-name = "fahim"
-print(name)
+# POS Tagging
+pos_tags = pos_tag(words)
+print(pos_tags)
